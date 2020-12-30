b1c179b380c31e00e4ecaf24eb07b5964d3788ed
CodingPheonix <agraw17107@gapps.uwcsea.edu.sg>
2020-12-30 15:51:25 +0800

    Remade scenes with ML-Agents soccer assets

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+//Assign this class to the unit that should be/can be controlled
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
