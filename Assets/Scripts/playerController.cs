4c56fb4637141c4db4c8163fc60415dfc0aa9409
CodingPheonix <agraw17107@gapps.uwcsea.edu.sg>
2021-01-02 16:56:33 +0800

    Fixed Colliders

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb = GetComponent<Rigidbody>();
+
+    //Camera should be child of the player
+
+    // Update is called once per frame
+    void Update()
+    {
+     float xAxis = Input.getInputAxis("Horizontal");
+     float yAxis = Input.getInputAxis("Vertical");   
+    }
+}
